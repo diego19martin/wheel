264f5d7d4f10c17f7fc8c6a0e727705310d71dca
Diego <diego19martin@hotmail.com>
2023-01-18 16:31:36 -0300

    accion-altas

--- script.js
+++ script.js
@@ -22,34 +22,35 @@ function spin(){
     let SelectedItem = "";
     spin.style.display='none';
 
-    let cincoMil = shuffle([1890, 2250, 2610]);
-    let diezMil = shuffle([1850, 2210, 2570]);
-    let  tucson = shuffle([1770, 2130, 2490]);
-    let  quinientos = shuffle([1810, 2170, 2530]);
-    let  sport = shuffle([1750, 2110, 2470]);
-    let  diezMilXC = shuffle([1630, 1990, 2350]);
-    let  mil = shuffle([1570, 1930, 2290]);
+    let cincoMil = shuffle([1770, 2130, 2490]);
+    let sieteMil = shuffle([1890, 2250, 2610]);
+    let  diezMil = shuffle([1630, 1990, 2350]);
+    let duplicaVeinte = shuffle([1570, 1930, 2290]);
+    let duplicaTreinte = shuffle([1810, 2170, 2530]);
+    let duplicaCincuenta = shuffle([1750, 2110, 2470]);
+    
+
    
 
     let results = shuffle([
-        cincoMil[5],
+        cincoMil[0],
+        sieteMil[0],
         diezMil[0],
-        tucson[0],
-        quinientos[0],
-        sport[0],
-        diezMilXC[0],
-        mil[0],
-       
+        duplicaVeinte[0],
+        duplicaTreinte[0],
+        duplicaCincuenta[0],
+      
     ])
 
     if(cincoMil.includes(results[0])) SelectedItem = "Bono $5.000 BetFun";
+    if(sieteMil.includes(results[0])) SelectedItem = "Bono $7.000 BetFun";
     if(diezMil.includes(results[0])) SelectedItem = "Bono $10.000 BetFun";
-    if(tucson.includes(results[0])) SelectedItem = "Cena en Tucson para dos personas";
-    if(quinientos.includes(results[0])) SelectedItem = "Bono $500 BetFun";
-    if(sport.includes(results[0])) SelectedItem = "SportBar para dos personas";
-    if(diezMilXC.includes(results[0])) SelectedItem = "10.000 XC";
-    if(mil.includes(results[0])) SelectedItem = "Bono $1.000 BetFun";
+    if(duplicaVeinte.includes(results[0])) SelectedItem = "Duplica hasta $20.000";
+    if(duplicaTreinte.includes(results[0])) SelectedItem = "Duplica hasta $30.000";
+    if(duplicaCincuenta.includes(results[0])) SelectedItem = "Duplica hasta $50.000";
     
+    console.log(results[0],SelectedItem);
+
     box.style.setProperty("transition", "all ease 5s");
     box.style.transform = "rotate(" + results[0] + "deg)";
     element.classList.remove("animate");
